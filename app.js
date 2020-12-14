8f93d8963cfba3f4605655d177be38b0b0a9f027
near2542 <double-decade1@hotmail.com>
2020-12-14 13:35:50 +0700

    Push

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const line = require('@line/bot-sdk');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 4000
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
-
+app.use('/image', express.static('image'));
 const channelAccessToken = process.env.access_token
 const channelSecret = process.env.channel_secret
 const config = {
@@ -37,30 +39,19 @@ app.post('/webhook',(req,res) =>
         type:'text',
         text:'its working',
     };
-
-    const message2 = {
-
-            type: "text",
-            text: "\uDBC0\uDC84 LINE original emoji",
-          
-    }
-    const message3 = 
-        {
-            "type": "image",
-            "originalContentUrl": "https://scontent-kut2-1.xx.fbcdn.net/v/t1.0-9/117892868_3165357106881401_2541502139840212849_n.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeF8F1dLVZbSM1o4hYsNHdhcE05lkXgQmnATTmWReBCacAeXgBbsqQgLwRomnfJGHbZn0P16xNItC6jxcUgTZA99&_nc_ohc=dO_JwQTJ6WsAX-Fh91m&_nc_ht=scontent-kut2-1.xx&oh=a55b878dbfccf2997b986bb335650ec0&oe=5FFD97DD",
-            "previewImageUrl": "https://scontent-kut2-1.xx.fbcdn.net/v/t1.0-9/117892868_3165357106881401_2541502139840212849_n.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeF8F1dLVZbSM1o4hYsNHdhcE05lkXgQmnATTmWReBCacAeXgBbsqQgLwRomnfJGHbZn0P16xNItC6jxcUgTZA99&_nc_ohc=dO_JwQTJ6WsAX-Fh91m&_nc_ht=scontent-kut2-1.xx&oh=a55b878dbfccf2997b986bb335650ec0&oe=5FFD97DD"
-        }
-          
     
-
-    client.replyMessage(request,[message,message2,message3])
+    client.replyMessage(request,message)
     .then(()=> console.log('work'))
-    .then(()=>{res.sendStatus(200)
+    .then(()=>{
                 console.log(`it's done`)})
-    .catch(err=>{res.sendStatus(400);
-                console.error(err);
-                })
+    .catch(err=>{
+        console.error(err);
+        res.sendStatus(400);})
    
+    
+    client.pushMessage(user,message).then(()=> {console.log('work on push'); })
+
+    res.sendStatus(200);
 
 })
 
