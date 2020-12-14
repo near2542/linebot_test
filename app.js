76f0e5f5e52569e80e455d2bd956c3828a3d97db
near2542 <double-decade1@hotmail.com>
2020-12-14 09:06:05 +0700

    TEST

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const line = require('@line/bot-sdk');
 const express = require('express');
 const app = express();
-app.use(express.bodyParser())
 const PORT = process.env.PORT || 4000
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
