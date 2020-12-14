c62307e75564f9cdf1f7c7aa8ca006aac951a1ec
near2542 <double-decade1@hotmail.com>
2020-12-14 11:31:01 +0700

    test new

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const line = require('@line/bot-sdk');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 4000
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
 
 const channelAccessToken = process.env.access_token
 const channelSecret = process.env.channel_secret
@@ -27,11 +29,6 @@ app.post('/webhook',(req,res) =>
     let request = req.body.events[0].replyToken
     let text = req.body.events[0].message.text
     let user = req.body.events[0].source.userId
-    console.log(`request is ${request} \n 
-                    text is ${text}\n
-                    user is ${user}`)
-   
-
 
     const message = {
         type:'text',
